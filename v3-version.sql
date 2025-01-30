8624fcc6d45ffc3343cd98cc0f882fe79e7fc5d2
Maria2910 <143892356+Maria2910@users.noreply.github.com>
2025-01-30 20:14:51 +0300

    Update v3-version.sql

--- v3-version.sql
+++ v3-version.sql
@@ -1,3 +1,14 @@
-/**
-  @author
- */
+
+--v2
+alter table human add column date_registration timestamp default current_timestamp; -- + timestamp
+alter table company add constraint unique_model unique (INN); -- + unique
+alter table human alter column name type varchar(36); -- varchar(255) -> varchar(36)
+alter table client add column order_cost integer default 0; --+ column integer
+alter table client alter column order_cost type numeric (10, 2) using order_cost::numeric (10, 2); --integer -> numeric(10, 2)
+
+--rollback
+alter table human drop column date_registration;
+alter table company drop constraint unique_model;
+alter table human alter column name type varchar(255);
+alter table client drop column order_cost;
+alter table client alter column order_cost type integer using order_cost::integer;
